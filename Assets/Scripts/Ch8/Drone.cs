a44523c6214184b2c0af81b6aa6a757694f04fe8
SamEBaker <esparzs1@my.erau.edu>
2024-09-25 13:27:27 -0700

    project

---
+++ Assets/Scripts/Ch8/Drone.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Pool;
+using UnityEngine;
+
+namespace Chapter.ObjectPool
+{
+    public class Drone : MonoBehaviour
+    {
+        public IObjectPool<Drone> Pool { get; set; }
+        public float _currentHealth;
+        [SerializeField]
+        private float maxHealth = 100.0f;
+        [SerializeField]
+        private float timeToSelfDestruct = 3.0f;
+        void Start()
+        {
+            _currentHealth = maxHealth;
+        }
+        void OnEnable()
+        {
+            AttackPlayer();
+            StartCoroutine(SelfDestruct());
+        }
+        void OnDisable()
+        {
+            ResetDrone();
+        }
+
+        IEnumerator SelfDestruct()
+        {
+            yield return new WaitForSeconds(timeToSelfDestruct);
+            TakeDamage(maxHealth);
+        }
+        private void ReturnToPool()
+        {
+            Pool.Release(this);
+        }
+        private void ResetDrone()
+        {
+            _currentHealth = maxHealth;
+        }
+        public void AttackPlayer()
+        {
+            Debug.Log("Attack player!");
+        }
+
+ public void TakeDamage(float amount)
+        {
+            _currentHealth -= amount;
+            if (_currentHealth <= 0.0f)
+                ReturnToPool();
+        }
+    }
+}
